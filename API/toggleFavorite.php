f4e52618544065030a1baea5afb809b5b0e69b31
iehara0054 <iehara0054@gmail.com>
2025-12-12 12:06:23 +0900

    claudaの設定

--- API/toggleFavorite.php
+++ API/toggleFavorite.php
@@ -1,10 +1,11 @@
 <?php
-// JSON形式で送られてきたデータを受け取る
-$json = file_get_contents('php://input');
-$data = json_decode($json, true);
+require_once __DIR__ . '../class/BookMarkManager.php';
+$BookMarkManager = new BookMarkManager;
 
-// idを取得
+$json = file_get_contents('php://input');
+// $data = json_decode($json, true);
 $itemId = $data['id'] ?? null;
 
-// 確認用の出力
-var_dump($itemId);
+$getJsonValue = $BookMarkManager->getJsonValue($BookMarkManager::BOOKMARKS_JSON_FILE, $itemId);
+
+var_dump($getJsonValue);
