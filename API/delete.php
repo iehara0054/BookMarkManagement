20b29ad0811c207a8de6d06adf48f812999dc97b
iehara0054 <iehara0054@gmail.com>
2026-01-08 10:56:10 +0900

    親フォルダの変更に伴うファイルの破棄

--- API/delete.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-header("Content-Type: application/json; charset=utf-8");
-
-require_once __DIR__ . '/../class/BookMarkManager.php';
-require_once __DIR__ . '/../class/Helper.php';
-$BookMarkManager = new BookMarkManager;
-$Helper = new Helper;
-
