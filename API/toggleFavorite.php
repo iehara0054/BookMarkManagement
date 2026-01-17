7f6a0063685163ccc296063319d85c7e40903ec6
iehara <shigeki@ack-st.co.jp>
2026-01-17 19:20:53 +0900

    Revert "toggleFavorite.phpでお気に入りをクリックしたjsonデータだけ取得する処理へ変更"
    This reverts commit 2977abecea1bda4d2e1bac2b8450a27390b39975.

--- API/toggleFavorite.php
+++ API/toggleFavorite.php
@@ -8,31 +8,27 @@ $BookMarkManager = new BookMarkManager;
 $Helper = new Helper;
 
 $posted = json_decode(file_get_contents('php://input'), true);
+$postedId = $posted['id'] ?? null;
 
-$targetKey = 'id';
-$targetValue = $posted['id'] ?? null;
-
-if ($targetKey === null)
+if ($postedId === null)
 {
     echo json_encode(['error' => 'ID is required']);
     exit;
 }
 
-$searchedBookMark = $BookMarkManager->search_bookmarks($targetKey, $targetValue);
+$getJsonData = file_get_contents(Helper::BOOKMARKS_JSON_FILE);
+$getJsonDataDecode = json_decode($getJsonData, true);
 
-// $getJsonData = file_get_contents(Helper::BOOKMARKS_JSON_FILE);
-// $getJsonDataDecode = json_decode($getJsonData, true);
+foreach ($getJsonDataDecode as $key => &$item)
+    {
+        if ($item['id'] === $postedId)
+        {
+            $item['favorite'] = !$item['favorite'];
+            break;
+        }
+    }
+unset($item);
 
-// foreach ($getJsonDataDecode as $key => &$item)
-//     {
-//         if ($item['id'] === $postedId)
-//         {
-//             $item['favorite'] = !$item['favorite'];
-//             break;
-//         }
-//     }
-// unset($item);
+$json = $BookMarkManager->save_bookMarks($getJsonDataDecode);
 
-// $json = $BookMarkManager->save_bookMarks($getJsonDataDecode);
-
-echo $json = json_encode(array_values($searchedBookMark), JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE);
+echo $json;
